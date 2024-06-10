375f2e3d2d1487a423fa3e48961a660883185a0f
Adrià Priego Garcia <apriego-@car7s4.42barcelona.com>
2024-06-10 16:57:11 +0200

    Add protaction and guide

--- create.sh
+++ create.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
+first=".first"
+
+if [ ! -f "$first" ];
+then
+    echo Forma de uso bash ./script.sh DIR DE LA CARPETA DESCOMPRIMIDA DEL VISUAL exemple: ~/Downloads/code-stable-x64-1715058993/VSCode-linux-x64.
+    echo -e "\e[33mEs importante la carpeta ha de ser la que tiene el ejecutable del propio visual si no es esa no funcionara no me hago responsable de lo que pase despues si no sigues de forma correcta todo."
+    echo -e "UP2YOU Usar este script.sh. Personalmente recomiendo hazerlo paso a paso y ya de paso entender como funciona todo este tema.\e[0m"
+    echo Usage: bash ./script.sh PATH_TO_THE_UNZIPPED_FOLDER_OF_VISUAL  exemple: ~/Downloads/code-stable-x64-1715058993/VSCode-linux-x64.
+    echo -e "\e[33mIt's important that the folder provided contains the executable of Visual Studio Code. If it's not the correct folder, it won't work. I'm not responsible for any issues that may occur if you don't follow these instructions correctly."
+    echo -e "UP2YOU to use this script.sh. Personally, I recommend doing it step by step and understanding how everything works.\e[0m"
+    touch $first
+    exit 1
+fi
+
 if [ -z "$1" ];
 then
     echo Forma de uso bash ./script.sh DIR DE LA CARPETA DESCOMPRIMIDA DEL VISUAL exemple: ~/Downloads/code-stable-x64-1715058993/VSCode-linux-x64.
@@ -30,3 +44,4 @@ chmod +x ~/.local/share/applications/vscode.desktop
 cp ~/.local/share/applications/vscode.desktop ~/Desktop
 chmod +x ~/Desktop/vscode.desktop
 echo DONE
+rm $first
